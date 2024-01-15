5cdaea9fc4053d81907fc83530942960e57d047d
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-15 17:05:53 +0530

    create Constructor for IConfiguration...

--- backend_/backend_/Controllers/RegistrationController.cs
+++ backend_/backend_/Controllers/RegistrationController.cs
@@ -8,5 +8,10 @@ namespace backend_.Controllers
     public class RegistrationController : ControllerBase
     {
         private readonly IConfiguration _configuration;
+
+        public RegistrationController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
     }
 }
