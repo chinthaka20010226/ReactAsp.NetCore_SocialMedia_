43cf433cbeb440c6fc460fa0339e6a21e896977d
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-15 16:20:34 +0530

    set all Classes and its Attributes...

---
+++ backend_/backend_/Models/Article.cs
@@ -0,0 +1,13 @@
+﻿namespace backend_.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public String Title { get; set; }
+        public String Content { get; set; }
+        public String Email { get; set; }
+        public String Image { get; set; }
+        public int IsActive { get; set; }
+        public int IsApproved { get; set; }
+    }
+}
---
+++ backend_/backend_/Models/Events.cs
@@ -0,0 +1,11 @@
+﻿namespace backend_.Models
+{
+    public class Events
+    {
+        public int Id { get; set; }
+        public String Title { get; set; }
+        public String Content { get; set; }
+        public String Email { get; set; }
+        public int IsActive { get; set; }
+    }
+}
