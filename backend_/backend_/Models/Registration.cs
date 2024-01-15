6267069813fb5b87d1fdc17efbf4635ebf342c75
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-15 16:03:41 +0530

    set Attributes for Registration...

--- backend_/backend_/Models/Registration.cs
+++ backend_/backend_/Models/Registration.cs
@@ -2,6 +2,12 @@
 {
     public class Registration
     {
-
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public String Password { get; set; }
+        public String PhoneNo { get; set; }
+        public int IsActive { get; set; }
+        public int IsApproved { get; set; }
     }
 }
