57c9ae770bb5c9a10a1723ef7dae1d897adcbc30
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-16 22:49:18 +0530

    StaffRegistration, DeleteStaff must in Registration controller because, its Registration, Delete by Admin...

--- backend_/backend_/Controllers/RegistrationController.cs
+++ backend_/backend_/Controllers/RegistrationController.cs
@@ -56,5 +56,31 @@ namespace backend_.Controllers
 
             return response;
         }
+
+        [HttpPost]
+        [Route("StaffRegistration")]
+
+        public Response StaffRegistartion(Staff staff)
+        {
+            Response response = new Response();
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("sa").ToString());
+            Dal dal = new Dal();
+            response = dal.StaffRegistration(staff, connection);
+
+            return response;
+        }
+
+        [HttpPost]
+        [Route("DeleteStaff")]
+
+        public Response DeleteStaff(Staff staff)
+        {
+            Response response = new Response();
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("sa").ToString());
+            Dal dal = new Dal();
+            response = dal.DeleteStaff(staff, connection);
+
+            return response;
+        }
     }
 }
