6465c1b228bdbfbb647a123b392f86195ca8b187
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-19 18:31:09 +0530

    Now, sql connection is okay...

---
+++ backend_/backend_/Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace backend_.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        
+    }
+}
