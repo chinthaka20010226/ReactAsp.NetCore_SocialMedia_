95f29a4a68303801ce5fd956143b12694ac81369
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-19 17:39:27 +0530

    error correction of Sql Query Command...

--- backend_/backend_/Models/Dal.cs
+++ backend_/backend_/Models/Dal.cs
@@ -8,7 +8,7 @@ namespace backend_.Models
         public Response Registration(Registration registration, SqlConnection connection)
         {
             Response response = new Response();
-            SqlCommand cmd = new SqlCommand("INSER INTO Registration(Name,Email,Password,PhoneNo,IsActive,IsApproved) VALUES('"+registration.Name+ "','"+registration.Email+ "','"+registration.Password+ "','"+registration.PhoneNo+"',1,0)",connection);
+            SqlCommand cmd = new SqlCommand("INSERT INTO Registration(Name,Email,Password,PhoneNo,IsActive,IsApproved) VALUES('"+registration.Name+ "','"+registration.Email+ "','"+registration.Password+ "','"+registration.PhoneNo+"',1,0)",connection);
             connection.Open();
             int i = cmd.ExecuteNonQuery();
             connection.Close();
