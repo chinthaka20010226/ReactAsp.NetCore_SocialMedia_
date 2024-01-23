2d1de82ab2f0a57b9acb8a5e7224d0357c874747
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-23 21:19:27 +0530

    side bar is now Okay...

--- frontend_/src/Components/Setting.js
+++ frontend_/src/Components/Setting.js
@@ -8,103 +8,86 @@ function Setting(){
                     <div className="col">
                         <div className="card">
                             <div className="row">
-                                <div className="col-2 d-sm-block d-none bg-info">
-                                    <div className="">
-                                        <a href="#" className="d-md-flex">
-                                            <div>
-                                                <i class="ri-dashboard-3-line"></i>                                        
+                                <div className="col-3 d-md-block d-none bg-info side-body">
+                                    <div className=""></div>
+                                    <div className="mt-5">
+                                        <a href="#" className="d-md-flex w-100 mx-1 my-2 p-1">
+                                            <div className="mx-2 icon-font">
+                                                <i class="ri-dashboard-3-line"></i>                                    
                                             </div>
-                                            <div>
+                                            <div className="d-flex justify-content-center align-items-center mx-1 icon-text">
                                                 <span>DASHBOARD</span>
                                             </div>
                                         </a>
-                                    </div>
-                                    <div className="">
-                                        <a href="#" className="d-md-flex">
-                                            <div>
-                                                <i class="ri-exchange-dollar-fill"></i>                                       
+                                        <a href="#" className="d-md-flex w-100 mx-1 my-2 p-1">
+                                            <div className="mx-2 icon-font">
+                                                <i class="ri-exchange-dollar-fill"></i>                                   
                                             </div>
-                                            <div>
+                                            <div className="d-flex justify-content-center align-items-center mx-1 icon-text">
                                                 <span>TRANSACTION</span>
                                             </div>
                                         </a>
-                                    </div>
-                                    <div className="">
-                                        <a href="#" className="d-md-flex">
-                                            <div>
-                                                <i class="ri-dashboard-3-line"></i>                                        
+                                        <a href="#" className="d-md-flex w-100 mx-1 my-2 p-1">
+                                            <div className="mx-2 icon-font">
+                                                <i class="ri-wallet-2-fill"></i>                                   
                                             </div>
-                                            <div>
+                                            <div className="d-flex justify-content-center align-items-center mx-1 icon-text">
                                                 <span>CARDS</span>
                                             </div>
                                         </a>
-                                    </div>
-                                    <div className="">
-                                        <a href="#" className="d-md-flex">
-                                            <div>
-                                                <i class="ri-dashboard-3-line"></i>                                        
+                                        <a href="#" className="d-md-flex w-100 mx-1 my-2 p-1">
+                                            <div className="mx-2 icon-font">
+                                                <i class="ri-bank-line"></i>                                   
                                             </div>
-                                            <div>
-                                                <span>BANK </span>
+                                            <div className="d-flex justify-content-center align-items-center mx-1 icon-text">
+                                                <span>BANK ACCOUNT</span>
                                             </div>
                                         </a>
-                                    </div>
-                                    <div className="">
-                                        <a href="#" className="d-md-flex">
-                                            <div>
-                                                <i class="ri-dashboard-3-line"></i>                                        
+                                        <a href="#" className="d-md-flex w-100 mx-1 my-2 p-1">
+                                            <div className="mx-2 icon-font">
+                                                <i class="ri-notification-4-line"></i>                                    
                                             </div>
-                                            <div>
+                                            <div className="d-flex justify-content-center align-items-center mx-1 icon-text">
                                                 <span>NOTIFICATION</span>
                                             </div>
                                         </a>
-                                    </div>
-                                    <div className="">
-                                        <a href="#" className="d-md-flex">
-                                            <div>
-                                                <i class="ri-dashboard-3-line"></i>                                        
+                                        <a href="#" className="d-md-flex w-100 mx-1 my-2 p-1">
+                                            <div className="mx-2 icon-font">
+                                                <i class="ri-line-chart-fill"></i>                                   
                                             </div>
-                                            <div>
+                                            <div className="d-flex justify-content-center align-items-center mx-1 icon-text">
                                                 <span>ANALYSIS</span>
                                             </div>
                                         </a>
-                                    </div>
-                                    <div className="">
-                                        <a href="#" className="d-md-flex">
-                                            <div>
-                                                <i class="ri-dashboard-3-line"></i>                                        
+                                        <a href="#" className="d-md-flex w-100 mx-1 my-2 p-1">
+                                            <div className="mx-2 icon-font">
+                                                <i class="ri-folder-chart-line"></i>                                   
                                             </div>
-                                            <div>
+                                            <div className="d-flex justify-content-center align-items-center mx-1 icon-text">
                                                 <span>REPORTS</span>
                                             </div>
                                         </a>
-                                    </div>
-                                    <div className="">
-                                        <a href="#" className="d-md-flex">
-                                            <div>
-                                                <i class="ri-dashboard-3-line"></i>                                        
+                                        <a href="#" className="d-md-flex w-100 mx-1 my-2 p-1">
+                                            <div className="mx-2 icon-font">
+                                                <i class="ri-arrow-right-circle-fill"></i>                                   
                                             </div>
-                                            <div>
+                                            <div className="d-flex justify-content-center align-items-center mx-1 icon-text">
                                                 <span>ALLOWANS</span>
                                             </div>
                                         </a>
-                                    </div>
-                                    <div className="">
-                                        <a href="#" className="d-md-flex">
-                                            <div>
-                                                <i class="ri-dashboard-3-line"></i>                                        
+                                        <a href="#" className="d-md-flex w-100 mx-1 my-2 p-1">
+                                            <div className="mx-2 icon-font">
+                                                <i class="ri-organization-chart"></i>                                  
                                             </div>
-                                            <div>
+                                            <div className="d-flex justify-content-center align-items-center mx-1 icon-text">
                                                 <span>ORGANIZATION</span>
                                             </div>
                                         </a>
-                                    </div>
-                                    <div className="">
-                                        <a href="#" className="d-md-flex">
-                                            <div>
-                                                <i class="ri-dashboard-3-line"></i>                                        
+                                        <a href="#" className="d-md-flex w-100 mx-1 my-2 p-1">
+                                            <div className="mx-2 icon-font">
+                                                <i class="ri-user-settings-line"></i>                                 
                                             </div>
-                                            <div>
+                                            <div className="d-flex justify-content-center align-items-center mx-1 icon-text">
                                                 <span>SETTINGS</span>
                                             </div>
                                         </a>
